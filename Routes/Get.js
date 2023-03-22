abf711b2b43d52c80c450720aad8d7c0a35abec6
Anderson De Cezaro <atendimento@armazenamentonfe.com.br>
2023-03-22 16:07:32 -0300

    Start program

---
+++ Routes/Get.js
@@ -0,0 +1,5 @@
+class ticket {
+    constructor(id) {
+        
+    }
+}
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+
+app.listen(PORT, () => {
+    console.log("Server listen on port 4000");
+});
